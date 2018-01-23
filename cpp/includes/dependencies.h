42ded85ba9769f2c21117dabe0c5ba253f209888
Nash Sheldon <parsemyarse@gmail.com>
2018-01-23 10:09:45 -0600

    Update dependencies.h

--- cpp/includes/dependencies.h
+++ cpp/includes/dependencies.h
@@ -1,13 +1,29 @@
 /////////////////////
-//The basic shit...//
+//  The basic shit...
 /////////////////////
 
 #ifndef _DEPENDS_H
 #define _DEPENDS_H
+#include <bitset>
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <regex>
+#include <iomanip>
 #include <cstdlib>
 #include <cstring>
-#include <string>
-#include <cstdio>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/tcp.h>   
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <vector>
+#include <iterator>
+#include <curl/curl.h>
 #include <time.h>
+#include <netdb.h>
 #endif
