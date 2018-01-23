338f104a81755dfbded8785f89329ef1c86f2844
Nash Sheldon <parsemyarse@gmail.com>
2018-01-23 10:08:17 -0600

    Create dependencies.h

---
+++ cpp/includes/dependencies.h
@@ -0,0 +1,13 @@
+/////////////////////
+//The basic shit...//
+/////////////////////
+
+#ifndef _DEPENDS_H
+#define _DEPENDS_H
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <cstdio>
+#include <time.h>
+#endif
