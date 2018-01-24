b43a3e6dd1e9fcec16416ee367866b2894aa6c3d
Nash Sheldon <parsemyarse@gmail.com>
2018-01-24 09:16:05 -0600

    Update log.h

--- cpp/includes/log.h
+++ cpp/includes/log.h
@@ -1,8 +1,6 @@
 #ifndef _LOG_H
 #define _LOG_H
-
 //  Definitions in /cpp/src/log.cpp
-
 extern int LOG_FD;
 __attribute__((constructor)) void init();   //  Opens the log file for reading and writing at runtime
 #endif
