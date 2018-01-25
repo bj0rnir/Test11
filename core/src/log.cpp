8c48206ee07a2ef51ccaa59bea4a759a8e04840b
Nash Sheldon <parsemyarse@gmail.com>
2018-01-25 08:55:03 -0600

    Update log.cpp

--- core/src/log.cpp
+++ core/src/log.cpp
@@ -1,5 +1,6 @@
 #include "log.h"
 std::ofstream LOG_FD;
+char CWD[1024];
 __attribute__((constructor)) void init(){   //  Opens the log file for reading and writing at runtime
   LOG_FD.open("arsenal.log", std::ofstream::out | std::ofstream::app);
 }
