63c3578ce1dab5078dd517d252469922f7eb587d
Nash Sheldon <parsemyarse@gmail.com>
2018-01-24 18:07:56 -0600

    Update log.h

--- cpp/includes/log.h
+++ cpp/includes/log.h
@@ -1,5 +1,6 @@
 #ifndef _LOG_H
 #define _LOG_H
+#include <fstream>
 //  Definitions in /cpp/src/log.cpp
 extern std::ofstream LOG_FD;
 __attribute__((constructor)) void init();   //  Opens the log file for reading and writing at runtime
