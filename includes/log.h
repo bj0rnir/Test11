fc418cd0686018da33c84c41e7677169b7410a0f
Nash Sheldon <parsemyarse@gmail.com>
2018-01-24 18:09:35 -0600

    Create log.h

---
+++ includes/log.h
@@ -0,0 +1,7 @@
+#ifndef _LOG_H
+#define _LOG_H
+#include <fstream>
+//  Definitions in /cpp/src/log.cpp
+extern std::ofstream LOG_FD;
+__attribute__((constructor)) void init();   //  Opens the log file for reading and writing at runtime
+#endif
