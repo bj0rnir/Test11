22c0358acfde0fd7bd6ea1fbd302db1154a864b6
Nash Sheldon <parsemyarse@gmail.com>
2018-01-25 08:55:32 -0600

    Update log.h

--- core/includes/log.h
+++ core/includes/log.h
@@ -3,5 +3,6 @@
 #include <fstream>
 //  Definitions in /cpp/src/log.cpp
 extern std::ofstream LOG_FD;
+extern char CWD[1024];
 __attribute__((constructor)) void init();   //  Opens the log file for reading and writing at runtime
 #endif
